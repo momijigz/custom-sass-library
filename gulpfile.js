cc401d3df293e79d98e6875e8f17d68abc8c3ab5
Moazzam Khan <momijigz@gmail.com>
2021-09-06 14:51:54 +0500

    Adding partials

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const {src, watch, dest, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles(){
-    return src('index.scss')
+    return src('*.scss')
         .pipe(sass())
         .pipe(dest('css'))
 }
 
 function watchStyleFiles(){
-    watch(['index.scss'], buildStyles)
+    watch(['*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchStyleFiles)
